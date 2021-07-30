23fa063f14af35e1926e6eb8cce7fe79397903ab
Moudou <dplayerd@gmail.com>
2021-07-30 13:12:05 +0800

    【修正】修正 Logger 不會自動開資料夾的錯誤

--- WebformMiniSample/AccountingNote.DBSource/Logger.cs
+++ WebformMiniSample/AccountingNote.DBSource/Logger.cs
@@ -16,7 +16,16 @@ namespace AccountingNote.DBSource
 
                 ";
 
-            System.IO.File.AppendAllText("D:\\Logs\\Log.log", msg);
+            string logPath = "D:\\Logs\\Log.log";
+            string folderPath = System.IO.Path.GetDirectoryName(logPath);
+
+            if (!System.IO.Directory.Exists(folderPath))
+                System.IO.Directory.CreateDirectory(folderPath);
+
+            if (!System.IO.File.Exists(logPath))
+                System.IO.File.Create(logPath);
+
+            System.IO.File.AppendAllText(logPath, msg);
 
             throw ex;
         }
