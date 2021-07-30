6fe3018b57b2559696755dad03c6a6b7a38be1a7
Moudou <dplayerd@gmail.com>
2021-07-30 14:42:22 +0800

    【重構】將登入、登出抽至共用函式

--- WebformMiniSample/AccountingNote.Auth/AuthManager.cs
+++ WebformMiniSample/AccountingNote.Auth/AuthManager.cs
@@ -22,7 +22,6 @@ namespace AccountingNote.Auth
                 return true;
         }
 
-
         /// <summary> 取得已登入的使用者資訊 (如果沒有登入就回傳 null) </summary>
         /// <returns></returns>
         public static UserInfoModel GetCurrentUser()
@@ -51,11 +50,53 @@ namespace AccountingNote.Auth
             return model;
         }
 
-
-        /// <summary> 清除登入 </summary>
+        /// <summary> 登出 </summary>
         public static void Logout()
         {
             HttpContext.Current.Session["UserLoginInfo"] = null;           
+        }
+
+        /// <summary> 嘗試登入 </summary>
+        /// <param name="account"></param>
+        /// <param name="pwd"></param>
+        /// <param name="errorMsg"></param>
+        /// <returns></returns>
+        public static bool TryLogin(string account, string pwd, out string errorMsg)
+        {
+            // check empty
+            if (string.IsNullOrWhiteSpace(account) || 
+                string.IsNullOrWhiteSpace(pwd))
+            {
+                errorMsg = "Account / PWD is required.";
+                return false;
+            }
+
+
+            // read db and check
+            var dr = UserInfoManager.GetUserInfoByAccount(account);
+
+            // check null
+            if (dr == null)
+            {
+                errorMsg = $"Account: {account} doesn't exists.";
+                return false;
+            }
+
+
+            // check account / pwd
+            if (string.Compare(dr["Account"].ToString(), account, true) == 0 &&
+                string.Compare(dr["PWD"].ToString(), pwd, false) == 0)
+            {
+                HttpContext.Current.Session["UserLoginInfo"] = dr["Account"].ToString();
+
+                errorMsg = string.Empty;
+                return true;
+            }
+            else
+            {
+                errorMsg = "Login fail. Please check Account / PWD.";
+                return false;
+            }
         }
     }
 }
