a2cc8a3f381c5948a7dfe53294a85cbbd3ffa723
Moudou <dplayerd@gmail.com>
2021-09-06 09:47:21 +0800

    【開發】角色管理

---
+++ WebformMiniSample/AccountingNote.DBSource/RoleManager.cs
@@ -0,0 +1,82 @@
+﻿using AccountingNote.ORM.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingNote.DBSource
+{
+    class RoleManager
+    {
+        /// <summary> 取得所有角色清單 </summary>
+        /// <returns></returns>
+        public static List<Role> GetRoleList()
+        {
+            try
+            {
+                using (ContextModel context = new ContextModel())
+                {
+                    var query = context.Roles;
+                    var list = query.ToList();
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+                return null;
+            }
+        }
+
+        /// <summary> 用名稱取得角色 </summary>
+        /// <param name="roleName"></param>
+        /// <returns></returns>
+        public static Role GetRoleByName(string roleName)
+        {
+            try
+            {
+                using (ContextModel context = new ContextModel())
+                {
+                    var query =
+                        (from item in context.Roles
+                         where item.RoleName == roleName
+                         select item);
+
+                    var obj = query.FirstOrDefault();
+                    return obj;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+                return null;
+            }
+        }
+
+        /// <summary> 用 id 取得角色 </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static Role GetRole(Guid id)
+        {
+            try
+            {
+                using (ContextModel context = new ContextModel())
+                {
+                    var query =
+                        (from item in context.Roles
+                         where item.ID == id
+                         select item);
+
+                    var obj = query.FirstOrDefault();
+                    return obj;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+                return null;
+            }
+        }
+    }
+}
